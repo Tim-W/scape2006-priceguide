149152f397ee3e563c03171a541c67a818875c28
Tim Wissel <twtimwissel5@gmail.com>
2015-07-01 20:52:08 +0200

    fixed findOne statements

--- client/tpl/home.js
+++ client/tpl/home.js
@@ -19,17 +19,49 @@ Template.home.helpers({
 });
 
 Template.home.viewmodel({
-    search: null,
-    offers: function () {
-        if(this.search()) {
-            var searchValue = new RegExp(this.search().toLowerCase(), 'i');
-            return Offers.find({item: searchValue}, {sort: {createdAt: -1}});
-        } else {
-            return Offers.find({}, {sort: {createdAt: -1}});
+        search: null,
+        offers: function () {
+            if (this.search()) {
+                var searchValue = new RegExp(this.search().toLowerCase(), 'i');
+                return Offers.find({item: searchValue}, {sort: {createdAt: -1}});
+            } else {
+                return Offers.find({}, {sort: {createdAt: -1}});
+            }
         }
-    }
-},
-'offers');
+    },
+    'offers');
+
+Template.home.onRendered(function () {
+    //this.autorun(function () {
+    //
+    //})
+    var initializing = true;
+    Offers.find().observeChanges({
+        added: function (id, doc) {
+            if (!initializing) {
+                var offer = Offers.findOne({_id: id});
+                if(Meteor.userId() !== offer.createdBy) {
+                    var createdBy = Meteor.users.findOne({_id: offer.createdBy});
+                    var buyingOrSelling;
+
+                    if (offer.buyingOrSelling === 'Buying') {
+                        buyingOrSelling = 'buying';
+                    } else {
+                        buyingOrSelling = 'selling';
+                    }
+
+                    var notification = new Notification('Scape-2006: new offer', {
+                        body: createdBy.username + ' is ' + buyingOrSelling + ' of ' + offer.amount + ' ' + offer.item + ' @ ' + offer.pricePerItem + ' GP ea'
+                    });
+
+                    setTimeout(notification.close.bind(notification), 4000);
+                }
+            }
+        }
+    });
+    initializing = false;
+})
+
 
 Template.home.events({
     'click #logout': function () {
